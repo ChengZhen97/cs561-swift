3d1a95256d0e947614984bce7ca4b7deb7032b3f
ChengZhen97 <90570127+ChengZhen97@users.noreply.github.com>
2022-02-27 22:41:04 -0800

    Revert "access secure endpoints (#1)"
    This reverts commit f744e61d21b42dfee0fa4cbb3d03dc5f159c81d1.

--- MyLibrary/Sources/MyLibrary/WeatherService.swift
+++ MyLibrary/Sources/MyLibrary/WeatherService.swift
@@ -2,48 +2,12 @@ import Alamofire
 
 public protocol WeatherService {
     func getTemperature(completion: @escaping (_ response: Result<Int /* Temperature */, Error>) -> Void)
-    func getHello(completion: @escaping (_ response: Result<String, Error>) -> Void)
-    func getAuth(completion: @escaping (_ authToken: String) -> Void)
 }
 
 class WeatherServiceImpl: WeatherService {
-    let urlWeather = "http://localhost:3000/v1/weather"
-    let urlAuth    = "http://localhost:3000/v1/auth"
-    let urlHello   = "http://localhost:3000/v1/hello"
+    let url = "https://api.openweathermap.org/data/2.5/weather?q=corvallis&units=imperial&appid=<INSERT YOUR API KEY HERE>"
 
-    func getAuth(completion: @escaping (_ auth: String) -> Void) -> Void) {
-        let params: Parameters = [
-            "username": "usr",
-            "password": "pass"
-        ]
-
-        AF.request(urlAuth, method: .post, parameters: params, encoding: JSONEncoding.default, headers: nil)
-          .validate(statusCode: 200..<300)
-          .responseDecodable(of: Token.self) {
-              AFdata in
-                do {
-                    guard let jsonObject = try JSONSerialization.jsonObject(with: AFdata.data!) as? [String: Any] else {
-                        print("Error: Cannot convert data to JSON object")
-                        return
-                    }
-                    let token = jsonObject["accessToken"] as! String
-                    completion(token)
-            } 
-    }
     func getTemperature(completion: @escaping (_ response: Result<Int /* Temperature */, Error>) -> Void) {
-        AF.request(url, method: .get).validate(statusCode: 200..<300).responseDecodable(of: Weather.self) { response in
-            switch response.result {
-            case let .success(weather):
-                let temperature = weather.main.temp
-                let temperatureAsInteger = Int(temperature)
-                completion(.success(temperatureAsInteger))
-
-            case let .failure(error):
-                completion(.failure(error))
-            }
-        }
-    }
-    func getHello(completion: @escaping (_ response: Result<String /* Temperature */, Error>) -> Void) {
         AF.request(url, method: .get).validate(statusCode: 200..<300).responseDecodable(of: Weather.self) { response in
             switch response.result {
             case let .success(weather):
